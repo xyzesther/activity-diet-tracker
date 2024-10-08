38ef391388edfc6dfd7b20ce223e088aaf37070a
xyzesther <zxyesther@gmail.com>
2024-10-08 15:19:10 -0700

    Add TabNavigator and screens for Activities, Diet, and Settings

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import ActivitiesScreen from './Screens/ActivitiesScreen';
+import DietScreen from './Screens/DietScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Activities') {
+              iconName = focused ? 'directions-run' : 'directions-run';
+            } else if (route.name === 'Diet') {
+              iconName = focused ? 'fastfood' : 'fastfood';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings';
+            }
+
+            return <MaterialIcons name={iconName} size={size} color={color}/>;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Activities" component={ActivitiesScreen} />
+        <Tab.Screen name="Diet" component={DietScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
