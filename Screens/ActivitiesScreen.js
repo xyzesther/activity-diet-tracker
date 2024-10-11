8e8e395057683e7faa7a9be536fbe1c273393703
xyzesther <zxyesther@gmail.com>
2024-10-10 21:54:27 -0700

    Replaced TouchOpacity component used in ActivitiesScreen and DietScreen with the reusable PressableButton

--- Screens/ActivitiesScreen.js
+++ Screens/ActivitiesScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, View, TouchableOpacity, Image } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
 import React, { useEffect } from 'react'
 import { spacing } from '../styles/styles'
 import ItemsList from '../Components/ItemsList'
 import { useTheme } from '../Components/ThemeContext';
+import PressableButton from '../Components/PressableButton';
 
 export default function ActivitiesScreen({ navigation }) {
   const { theme } = useTheme();
@@ -11,17 +12,19 @@ export default function ActivitiesScreen({ navigation }) {
     navigation.setOptions({
       title: 'Activities',
       headerRight: () => (
-        <TouchableOpacity
-          onPress={() => {
+        <PressableButton
+          pressedFunction={() => {
             navigation.navigate('Add An Activity')
           }}
+          style={styles.button}
+          pressedStyle={styles.pressedButton}
         >
           <Image
             source={require('../assets/add.png')}
             alt='an add button'
             style={styles.image}
           />
-        </TouchableOpacity>
+        </PressableButton>
       ),
     })
   }, [navigation])
@@ -37,6 +40,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: spacing.large,
+  },
+  button: {
+    marginRight: spacing.small,
+  },
+  pressedButton: {
+    opacity: 0,
   },
   image: {
     width: 20,
