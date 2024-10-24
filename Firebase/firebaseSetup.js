123ee63908ab2f6c5014030c5e0ef19572dd4f1e
xyzesther <zxyesther@gmail.com>
2024-10-23 18:04:27 -0700

    Fixed the wrong path for storageBucket, messagingSenderId, and appId

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -8,9 +8,9 @@ const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_apiKey,
   authDomain: process.env.EXPO_PUBLIC_authDomain,
   projectId: process.env.EXPO_PUBLIC_projectId,
-  storageBucket: EXPO_PUBLIC_storageBucket,
-  messagingSenderId: EXPO_PUBLIC_messagingSenderId,
-  appId: EXPO_PUBLIC_appId
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId
 };
 
 // Initialize Firebase
